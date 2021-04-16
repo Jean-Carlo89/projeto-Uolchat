cc6619c5d6ec274d1b3809cdc5ddca605f7b7558
Jean Carlo <jeancarlomo@hotmail.com>
2021-04-16 10:16:29 -0300

    ajustes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let log, newMessages;
 let userLog;
 let recipient='todos';
 let type = 'message'
-/*----------------Entrando na sala----------------*/
+/*----------------Enter room----------------*/
 
 login()
 function login(){
@@ -15,8 +15,6 @@ const enter = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol
 enter.then(loginSuccess,userLog)
 
 enter.catch(loginError)
-
-
 }
 
 function loginSuccess(AnswerSuccess){
@@ -26,6 +24,7 @@ function loginSuccess(AnswerSuccess){
     newMessages = setInterval(getMessages,3000)
     log = setInterval(stayLogged,5000,userLog) 
 }
+
 
 function loginError(AnswerError){
     console.log(AnswerError)
@@ -42,126 +41,73 @@ function loginStop(){
 
 function stayLogged(userLog){
     const stayMessage = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status',{name: userLog})
-
     stayMessage.then(stayCheck)
     stayMessage.catch(stayError)
 }
 
-function stayCheck(stay){
-   // console.log(stay)
-    //console.log('ainda on')
-}
+function stayCheck(stay){}
 
 function stayError(stayError){
-    console.log(stayError.response)
     alert('Você foi desconectado')
     window.location.reload()
-    //login()
-    //console.log('Errou')
 }
-
-
-
-
-
 
 /*--------------------------Get messages---------------*/
 
 function getMessages(){
-//conversation.innerHTML=""
-//console.log('atualizou as msg')
 const getMessages = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages')
-
 getMessages.then(check)
-
 }
 
 function check(messages){
     conversation.innerHTML=""
-    //console.log(messages)
-    /*const from =messages.data[0].from
-    const to =messages.data[0].to
-    const text =messages.data[0].text
-    const time =messages.data[0].time
-    const type =messages.data[0].type
-   console.log(from, to , text , time, type)*/
-   
-   //console.log(messages.data[messages.data.length-1])
- 
-   for(i=0;i<messages.data.length;i++){
+    
+    for(i=0;i<messages.data.length;i++){
         
-    const from =messages.data[i].from
-    const to =messages.data[i].to
-    const text =messages.data[i].text
-    const time =messages.data[i].time
-    const type =messages.data[i].type
-    
-    
-    if(type==='message'){
-    
-    conversation.innerHTML +=
-        `
-        <li class="message">
-           <p><span>(${time}) &nbsp <strong>${from}</strong> &nbsp para &nbsp <strong>${to}</strong>:</span> &nbsp ${text}</p>
-        </li>
+        const from =messages.data[i].from
+        const to =messages.data[i].to
+        const text =messages.data[i].text
+        const time =messages.data[i].time
+        const type =messages.data[i].type
         
-        `
-    }
-
-    if(type==='status'){
+        
+        if(type==='message'){
+        
         conversation.innerHTML +=
-        `
-        <li class="message action">
-        <p>(${time})&nbsp <strong>${from}</strong> &nbsp ${text}</p>
-        </li>
-        
-        `
-    }
+            `
+            <li class="message">
+                <p><span>(${time}) &nbsp <strong>${from}</strong> &nbsp para &nbsp <strong>${to}</strong>:</span> &nbsp ${text}</p>
+            </li>
+            
+            `
+        }
 
-    if(type==='private_message' && (from===userLog || to===userLog)){
-        conversation.innerHTML +=
-        `
-        <li class="message private">
-           <p><span>(${time}) &nbsp <strong>${from}</strong> &nbsp reservadamente &nbsp para &nbsp <strong>${to}</strong>:</span> &nbsp ${text}</p>
-        </li>
-        
-        `
-    }
+        if(type==='status'){
+            conversation.innerHTML +=
+            `
+            <li class="message action">
+                <p>(${time})&nbsp <strong>${from}</strong> &nbsp ${text}</p>
+            </li>
+            
+            `
+        }
+
+        if(type==='private_message' && (from===userLog || to===userLog)){
+            conversation.innerHTML +=
+            `
+            <li class="message private">
+            <p><span>(${time}) &nbsp <strong>${from}</strong> &nbsp reservadamente &nbsp para &nbsp <strong>${to}</strong>:</span> &nbsp ${text}</p>
+            </li>
+            
+            `
+        }
     }
    conversation.scrollTop =conversation.scrollHeight;   
-    //document.documentElement.scrollTop = document.documentElement.scrollHeight;
 }
 
-
-
-
- 
-    
-
-
-
-
-/*`
-<li class="message">
-   <span>(${time})</span> &nbsp <span><strong>${from}</strong></span> &nbsp para &nbsp <span><strong>${to}</strong>:</span> &nbsp ${text}
-</li>
-
-`*/
-
-
-/*----------------------------------------Enviar mensages-----*/
+/*----------------------------------------Send messages-----*/
 function sendMessage(){
    
-    /* const conversation = document.querySelector('ul');
-    let message = document.querySelector('input').value
-    conversation.innerHTML +=
-    `
-    <li class="message">
-        ${message}
-    </li
-    
-    `
-    message="";*/
     const removeAllChecks = document.querySelectorAll('.icon-check.selected')
     
     for(let i=0;i<removeAllChecks.length;i++){
@@ -180,7 +126,6 @@ function sendMessage(){
 
      sendMessage.then(refreshMessages)
      sendMessage.catch(reloadPage)
-
 }
 
 function refreshMessages(messageSent){
@@ -191,31 +136,19 @@ function reloadPage(messageNotSent){
     window.location.reload()
 }
 
-
-/*-------------Funcoes do bonus*/
+/*-------------Control active users and visibility type of message---------*/
 
 function openParticipants(){
     
     const body = document.querySelector('body')
-    /*body.innerHTML +=`
-    <div class="sidebar-container move" onclick="goBack(this)">
-            <div class="sidebar">
-
-            </div>
-    </div>
-    `*/
     const sidebar = document.querySelector('.sidebar-container')
     sidebar.classList.toggle('hidden')
     sidebar.classList.toggle('move')
-    
 }
 
 function goBack(page){
-    //console.log('click')
-   // console.log(page.parentNode)
-    page.parentNode.classList.toggle('hidden')
+   page.parentNode.classList.toggle('hidden')
    page.parentNode.classList.toggle('move')
-   
 }
 
 
@@ -223,21 +156,15 @@ function goBack(page){
 
 function getMessageName(clickedParticipant){
     const removeSelected = document.querySelector('.participant-option .icon-check.selected')
-    //console.log(removeSelected)
-    if(removeSelected===null){
-
-    }else{
-    removeSelected.classList.remove('selected')
+    
+    if(removeSelected!==null){
+        removeSelected.classList.remove('selected')
     }
+    
     clickedParticipant.children[1].classList.add('selected')
-    //console.log(clickedParticipant.children)
-   // console.log(clickedParticipant.children[0].innerText)
-   const personName = clickedParticipant.children[0].innerText
-   //console.log(personName)
-   recipient=personName
-   //clickedParticipant.children[0].children[0].setAttribute('selected','selected')
-   //console.log(clickedParticipant.children[1])
-   
+    const personName = clickedParticipant.children[0].innerText
+    
+    recipient=personName
 }
 
 function getListOfPeople(){
@@ -246,43 +173,36 @@ function getListOfPeople(){
 }
 
 function completeList(listOfPeople){
-   // console.log(listOfPeople)
-    //console.log(listOfPeople.data)
-    //console.log(listOfPeople.data[0].name)
+   
     const peopleUl=document.querySelector('.participants')
-    //console.log(peopleUl)
-
+    
     peopleUl.innerHTML =
     `<li class="participant-option" onclick="getMessageName(this)">
             <span> <ion-icon name="people-sharp" class="icon"></ion-icon>  Todos</span> <ion-icon name="checkmark-circle-outline" class="icon icon-check selected"></ion-icon>
     </li>
     `
 
+
     for(let i =0;i<listOfPeople.data.length;i++){
         peopleUl.innerHTML +=`
         <li class="participant-option" onclick="getMessageName(this)">
-            <span> <ion-icon name="people-sharp" class="icon"></ion-icon>  ${listOfPeople.data[i].name}</span> <ion-icon name="checkmark-circle-outline" class="icon icon-check"></ion-icon>
+            <span> <ion-icon name="person-circle" class="icon"></ion-icon>  ${listOfPeople.data[i].name}</span> <ion-icon name="checkmark-circle-outline" class="icon icon-check"></ion-icon>
         </li>
         `
     }
-
-    //console.log('lista att')
 }
 
 function getVisibilityType(clickedVisibility){
     const removeSelected = document.querySelector('.visibility-option .icon-check.selected')
-    //console.log(removeSelected)
-    if(removeSelected===null){
-
-    }else{
-    removeSelected.classList.remove('selected')
+    
+    if(removeSelected!==null){
+        removeSelected.classList.remove('selected')
     }
+    
     clickedVisibility.children[1].classList.add('selected')
-    //console.log(clickedVisibility.children)
-   // console.log(clickedVisibility.children[0].innerText)
-   let visibility = clickedVisibility.children[0].innerText
-   //console.log(visibility)
-    //console.log(typeof(visibility))
+   
+    let visibility = clickedVisibility.children[0].innerText
+   
    if(visibility=='Reservadamente'){
        visibility='private_message'
    }
@@ -293,13 +213,4 @@ function getVisibilityType(clickedVisibility){
    console.log(visibility)
    
    type=visibility
-
-
-   
-   //console.log(type)
-
-
-  // type=visibility
-   //clickedVisibility.children[0].children[0].setAttribute('selected','selected')
-   //console.log(clickedVisibility.children[1])
 }
